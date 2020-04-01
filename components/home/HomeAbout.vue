777b24013c8d4165efb8cf69dd4a17a85c0ec1f2
Sowa Killian <killian.sowa@m6.fr>
2020-04-01 20:56:29 +0200

    let's create OK

--- components/home/HomeAbout.vue
+++ components/home/HomeAbout.vue
@@ -2,7 +2,7 @@
 <template>
   <div class="home-about" id="lea">
     <span class="home-about-marquee">
-      Let's create
+      Let's create Let's create
     </span>
     <h2 v-scroll-reveal ="{
       distance: 0,
